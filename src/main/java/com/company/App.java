74a1f52ca0fc11521fc4f0dd389286b2ebc44ef5
szniemiec <szniemiec@yahoo.com>
2020-10-06 13:49:06 +0200

    Bad commit

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import com.company.connector.DatabaseCredentials;
+import com.company.connector.JSONService;
+import com.company.connector.PostgreSQLJDBC;
 import com.company.fillers.DatabaseFiller;
 
 /**
@@ -11,6 +14,11 @@ public class App
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         DatabaseFiller dbFiller = new DatabaseFiller();
-        dbFiller.fill();
+//        dbFiller.fill();
+
+        JSONService jsonService = new JSONService();
+        DatabaseCredentials databaseCredentials = jsonService.readEnvironment();
+        PostgreSQLJDBC database = new PostgreSQLJDBC();
+        database.connect(databaseCredentials);
     }
 }
--- src/main/java/com/company/fillers/DatabaseFiller.java
+++ src/main/java/com/company/fillers/DatabaseFiller.java
@@ -1,23 +1,73 @@
 package com.company.fillers;
 
+import com.company.loader.LoaderCsv;
+import com.company.loader.LoaderTxt;
+
+import java.io.IOException;
 import java.math.BigInteger;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DatabaseFiller {
 
-    public void fill() {
-        createCustomersEntry();
+    public void fillCustomers() throws IOException {
+        LoaderCsv loaderCsv = new LoaderCsv();
+        LoaderTxt loaderTxt = new LoaderTxt();
+
+        List<String[]> namesList, surnamesList, addressesList;
+
+        loaderCsv.setPath("src/main/resources/addresses.csv");
+        addressesList = loaderCsv.load();
+        loaderTxt.setPath("src/main/resources/first_names.txt");
+        namesList = loaderTxt.load();
+        loaderTxt.setPath("src/main/resources/last_names.txt");
+        surnamesList = loaderTxt.load();
+
+        String login = generateRandomLogin();
+        String password = generateRandomPassword();
+        String firstName = getRandomNameOrSurname(namesList);
+        String secondName = getRandomNameOrSurname(namesList);
+        String surname = getRandomNameOrSurname(surnamesList);
+        String birthday = generateRandomBirthday();
+
+        final String INSERT_SQL = "INSERT INTO \"Customers\" (\"Login\", \"Password\", " +
+                "\"First_Name\", \"Second_Name\", \"Surname\", \"Birthday\") " +
+                "VALUES (?, ?, ?, ?, ?, ?);";
+
+
     }
 
-    private void createCustomersEntry() {
-        final String INSERT_SQL = "INSERT INTO customers () " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
-        BigInteger id;
-        String login, password, firstName, secondName, surname, birthday;
-
+    private String getRandomNameOrSurname(List<String[]> list) {
+        int randomIndex = getRandomIndex(list.size());
+        return list.get(randomIndex)[0];
     }
+
+    private String generateRandomLogin() {
+        String numbers = "0123456789";
+        StringBuilder sb = generateString(numbers);
+        return sb.toString();
+    }
+
+    private String generateRandomPassword() {
+        String characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = generateString(characters);
+        return sb.toString();
+    }
+
+    private StringBuilder generateString(String characters) {
+        char[] numbersArr = characters.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int randomIndex;
+        for (int i = 0; i < 8; i++) {
+            randomIndex = getRandomIndex(numbersArr.length);
+            sb.append(numbersArr[randomIndex]);
+        }
+        return sb;
+    }
+
 
     private String generateRandomBirthday() {
         long ms;
@@ -26,6 +76,29 @@ public class DatabaseFiller {
         Date dt = new Date(ms);
         Timestamp ts = new Timestamp(dt.getTime());
         return ts.toString().split(" ")[0];
+    }
+
+    private int getRandomIndex(int max) {
+        return ThreadLocalRandom.current().nextInt(0, max);
+    }
+
+    private String[] getRandomAddress(List<String[]> list) {
+        int randomIndex = getRandomIndex(list.size());
+        String[] randomAddress = list.get(randomIndex);
+        String[] exactAddress = new String[2];
+        String address = randomAddress[1];
+        String zipcode = randomAddress[6];
+        exactAddress[0] = address;
+        exactAddress[1] = zipcode;
+        return exactAddress;
+    }
+
+    private void createCustomersEntry() {
+        final String INSERT_SQL = "INSERT INTO customers () " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+        BigInteger id;
+        String login, password, firstName, secondName, surname, birthday;
+
     }
 
 
