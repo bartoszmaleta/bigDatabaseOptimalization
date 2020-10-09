dc5aec9a7ac0654185c44a10043332a50624e091
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-10-09 06:11:26 +0200

    Add index on firstname in customer table + fix typo in transaction index

--- src/main/resources/indexQueries/create_index_and_select_transaction_by_value.sql
+++ src/main/resources/indexQueries/create_index_and_select_transaction_by_value.sql
@@ -2,7 +2,7 @@ SELECT * FROM "Transactions" WHERE "Value" = '4';
 
 EXPLAIN SELECT * FROM "Transactions" WHERE "Value" = '4';
 
-CREATE INDEX firstname_idx ON "Customers"("First_Name");
+CREATE INDEX transactions_value_idx ON "Transactions"("Value");
 
 CREATE OR REPLACE FUNCTION explain_select_transaction_by_value()
     RETURNS TEXT AS $explain_select_transaction_by_value$
